df24fa3888cba0f9494050bd27324178434a24fa
Shayan Azizi <80700762+shayan-azizi@users.noreply.github.com>
2021-10-03 17:51:24 +0330

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Import librarys
+from tkinter import *
+from tkinter import messagebox
+
+# Build main loop & canvas
+root = Tk()
+root.title("Lipbir - Tic Tac Toe!")
+
+b1 = Button(root, text= " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystenButtonFace", command = lambda: b_click(b1) )
+
+
+
+
+
+root.mainloop()
