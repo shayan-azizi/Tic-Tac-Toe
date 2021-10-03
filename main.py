73a123fbde11002fec7ea8950f08f90a72dec0a1
Shayan Azizi <80700762+shayan-azizi@users.noreply.github.com>
2021-10-03 20:33:21 +0330

    release 1.1

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from tkinter import messagebox
 clicked = True
 count = 0
 
+
 # Disable all buttons
 def disable_all_buttons ():
     b1.config(state = DISABLED)
@@ -166,7 +167,11 @@ def checkifwon ():
         winner = True
         messagebox.showinfo("Lipibr - Tic Tac Toe", "CONGRATULATIONS! O Wins!!")
         disable_all_buttons ()
-
+        
+    # Check if Tie
+    if count == 9 and winner == False:
+        messagebox.showinfo("Lipbir Tic Tac Toe", "Its A Tie!!\nNo one Win!")
+        disable_all_buttons()
 
 # Build main loop & canvas
 root = Tk()
@@ -189,32 +194,48 @@ def b_click (b):
     else:
         messagebox.showerror("Lipbir - Tic Tac Toe", "That box has already been selected\nPick antther box...")
         
+# Reset Function
+def reset ():
+    global b1, b2, b3, b4, b5, b6, b7, b8, b9
+    global clicked
+    global count
+    clicked = True
+    count = 0
+    
+    b1 = Button(root, text= " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b1) )
+    b2 = Button(root, text= " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b2) )
+    b3 = Button(root, text= " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b3) )
 
-b1 = Button(root, text= " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b1) )
-b2 = Button(root, text= " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b2) )
-b3 = Button(root, text= " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b3) )
+    b4 = Button(root, text= " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b4) )
+    b5 = Button(root, text= " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b5) )
+    b6 = Button(root, text= " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b6) )
 
-b4 = Button(root, text= " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b4) )
-b5 = Button(root, text= " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b5) )
-b6 = Button(root, text= " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b6) )
+    b7 = Button(root, text= " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b7) )
+    b8 = Button(root, text= " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b8) )
+    b9 = Button(root, text= " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b9) )
 
-b7 = Button(root, text= " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b7) )
-b8 = Button(root, text= " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b8) )
-b9 = Button(root, text= " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b9) )
+    # Grid our buttons to screen
+    b1.grid(row= 0, column=0)
+    b2.grid(row= 0, column=1)
+    b3.grid(row= 0, column=2)
 
-# Grid our buttons to screen
-b1.grid(row= 0, column=0)
-b2.grid(row= 0, column=1)
-b3.grid(row= 0, column=2)
+    b4.grid(row= 1, column=0)
+    b5.grid(row= 1, column=1)
+    b6.grid(row= 1, column=2)
 
-b4.grid(row= 1, column=0)
-b5.grid(row= 1, column=1)
-b6.grid(row= 1, column=2)
+    b7.grid(row= 2, column=0)
+    b8.grid(row= 2, column=1)
+    b9.grid(row= 2, column=2)
 
-b7.grid(row= 2, column=0)
-b8.grid(row= 2, column=1)
-b9.grid(row= 2, column=2)
+# Create Menu
+my_menu = Menu(root)
+root.config(menu = my_menu)
 
+# Crete Options menu
+options_menu = Menu(my_menu, tearoff = False)
+my_menu.add_cascade(label = "Options", menu = options_menu)
+options_menu.add_command(label = "Play Again", command = reset)
 
+reset()
 
 root.mainloop()
